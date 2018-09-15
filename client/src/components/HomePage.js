db60f59262ef05f46e7e71594a763909d68f58a8
Silvia <silviawanlin@gmail.com>
2018-09-15 11:43:44 -0400

    updated homepage from master

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-
-const HomePageHeading= ({ mobile }) => (
-  <Container text>
-    <Header
-      as='h1'
-      content='WeCook'
-      inverted
-      style={{
-        fontSize: mobile ? '2em' : '4em',
-        fontWeight: 'normal',
-        marginBottom: 0,
-        marginTop: mobile ? '1.5em' : '3em',
-      }}
-    />
-    <Header
-      as='h2'
-      content='Enter your search: '
-      inverted
-      style={{
-        fontSize: mobile ? '1.5em' : '1.7em',
-        fontWeight: 'normal',
-        marginTop: mobile ? '0.5em' : '1.5em',
-      }}
-    />
-    <Button primary size='huge'>
-      Search it
-      <Icon name='right arrow' />
-    </Button>
-  </Container>
-)
-
-HomepageHeading.propTypes = {
-  mobile: PropTypes.bool,
-}
-=======
 import React, { Component } from 'react';
 import { Input } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
@@ -53,4 +15,3 @@ class HomePage extends Component {
 }
 
 export default HomePage;
->>>>>>> 7afe12283bebe90978b43df6a8e1881e8e7af26f
